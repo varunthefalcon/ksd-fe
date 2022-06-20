3e12a76daf6818105ed50bd56aeac7ff42d956a8
varun <s.varun1054@gmail.com>
2022-06-20 16:15:12 +0530

    sidebar list done

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,8 @@
 import '../../assets/scss/sidebar.scss'
 import logo from '../../assets/media/logo-2.png'
 import { Link } from 'react-router-dom'
+import { FaHome, FaUsers, FaCar, FaFileInvoiceDollar, FaUserClock, FaSignOutAlt } from 'react-icons/fa';
+import { GiMechanicGarage } from 'react-icons/gi'
 
 const Sidebar = () => {
     return <div className="sidebar-container full-height">
@@ -8,27 +10,27 @@ const Sidebar = () => {
             <img src={logo} alt="logo" width="100%" />
         </div>
         <div className='sidebar-items'>
-            <div>
-                <Link to="/" >Home</Link>
-            </div>
-            <div>
-                <Link to="/" >Customer</Link>
-            </div>
-            <div>
-                <Link to="/" >Vehicles</Link>
-            </div>
-            <div>
-                <Link to="/" >Invoices</Link>
-            </div>
-            <div>
-                <Link to="/" >Inventory</Link>
-            </div>
-            <div>
-                <Link to="/" >Staffs</Link>
-            </div>
-            <div>
-                <Link to="/" >Signout</Link>
-            </div>
+            <Link to="/" >
+                <div className='active' > <FaHome />Home</div>
+            </Link>
+            <Link to="/customers" >
+                <div><FaUsers /> Customers </div>
+            </Link>
+            <Link to="/" >
+                <div><FaCar /> Vehicles</div>
+            </Link>
+            <Link to="/" >
+                <div><FaFileInvoiceDollar /> Invoices</div>
+            </Link>
+            <Link to="/" >
+                <div><GiMechanicGarage />Inventory</div>
+            </Link>
+            <Link to="/" >
+                <div><FaUserClock />Staffs</div>
+            </Link>
+            <Link to="/" >
+                <div><FaSignOutAlt />Signout</div>
+            </Link>
         </div>
     </div>
 }
